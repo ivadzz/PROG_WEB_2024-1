438b3a7dcd35628d0686f4ad05dc1c1f70440f0b
DAVI BRITO SILVA E SOUZA <davi.brito@a.ucb.br>
2024-03-20 11:01:15 -0300

    aula3

---
+++ exemplos/aula3/objeto.js
@@ -0,0 +1,12 @@
+
+const pessoa{
+cpf : 123456789;
+telefone : 61999999999;
+}
+
+for(const key in pessoa){
+    console.log(key + '-' + pessoa[key]);
+}
+
+let arr = [0,1,2,11,56,60];
+console.log(arr);
