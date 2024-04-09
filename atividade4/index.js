64bd58dd275dde562e7e11d09fb1f17d1fbbf13a
DAVI BRITO SILVA E SOUZA <davi.brito@a.ucb.br>
2024-04-09 10:08:55 -0300

    at4

--- atividade4/index.js
+++ atividade4/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const calculadora = require('./util/calculadora');
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.get('/somar/:a/:b', (req, res) => {
     const a = parseFloat(req.params.a);
