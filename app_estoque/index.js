bcfd35664f79702ec80e7e1572c744c1214216a7
ivadzz <vdavi205@gmail.com>
2024-04-11 15:12:42 -0300

    estoque

--- app_estoque/index.js
+++ app_estoque/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const estoque = require('./src/estoque');
+
 const app = express();
 
 app.get('/adicionar/:id/:nome/:qtd', function (req, res) {
@@ -13,12 +14,23 @@ app.get('/adicionar/:id/:nome/:qtd', function (req, res) {
 
 app.get('/listar', function (req, res) {
     res.send(estoque.listarProdutos());
-})
+});
 
 app.get('/remover/:id', function (req, res) {
     let id = req.params.id;
     estoque.removerProduto(id);
     res.send(id);
+});
+
+app.get('/editar/:id/:qtd', function (req, res) {
+    const id = req.params.id;
+    const novaQuantidade = req.params.qtd;
+    const editadoComSucesso = estoque.editarProduto(id, novaQuantidade);
+    if (editadoComSucesso) {
+        res.send(`Quantidade do produto ${id} editada para ${novaQuantidade}.`);
+    } else {
+        res.status(404).send('Produto não encontrado.');
+    }
 });
 
 const PORT = 8080;
--- app_estoque/src/estoque.js
+++ app_estoque/src/estoque.js
@@ -24,9 +24,20 @@ function removerProduto(id) {
     });
 }
 
+function editarProduto(id, novaQuantidade) {
+    const produtoIndex = produtos.findIndex((p) => p.id === id);
+    if (produtoIndex !== -1) {
+        produtos[produtoIndex].qtd = novaQuantidade;
+        return true;
+    } else {
+        return false;
+    }
+}
+
 module.exports = {
     criarProduto,
     adicionarProduto,
     listarProdutos,
-    removerProduto
+    removerProduto,
+    editarProduto
 };
